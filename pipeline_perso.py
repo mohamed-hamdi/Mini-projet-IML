bd81e10c8184bd44e149d6762fe2194f54eb168f
mohamed-hamdi <med.hamdi.1997@gmail.com>
2020-11-25 21:51:57 +0100

    First draft of the code

---
+++ pipeline_perso.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Functions used in a machine learning workflow
+implemented for a project at IMT Atlantique
+(Brest ,FRANCE)
+"""
+
+__author__ = "HAMDI Mohamed, KLEIMAN Ilan, OUALI Maher, RAJHI Mohamed"
+
+
+def function():
+    """Returns description .
+
+    Arguments and function description
+
+    @author : Author of the function
+    """
+    # Code here
+
+    return
+
+
