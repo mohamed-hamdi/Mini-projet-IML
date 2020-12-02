f7190d6d2057db600c0903d97657fb2803a691e4
mohamed-hamdi <med.hamdi.1997@gmail.com>
2020-12-02 10:53:49 +0100

    custom imputer

--- pipeline_perso.py
+++ pipeline_perso.py
@@ -44,38 +44,6 @@ def PlotCorrMatrix(data, FigSize=(10, 10)):
     plt.show()
     return
 
-def is_scalar_nan(x):
-    
-    """is_scalar_nan(x) is True does not fail.
-
-    @author : RAJHI Mohamed
-    """
-    return bool(isinstance(x, numbers.Real) and np.isnan(x))
-
-def _object_dtype_isnan(X):
-    """@author : RAJHI Mohamed"""
-    return X != X
-
-def _get_mask(X, value_to_mask):
-    """Compute the boolean mask X == value_to_mask.
-       @author : RAJHI Mohamed
-    """
-    if is_scalar_nan(value_to_mask):
-        if X.dtype.kind == "f":
-            return np.isnan(X)
-        elif X.dtype.kind in ("i", "u"):
-            # can't have NaNs in integer array.
-            return np.zeros(X.shape, dtype=bool)
-        else:
-            # np.isnan does not work on object dtypes.
-            return _object_dtype_isnan(X)
-    else:
-        return X == value_to_mask
-
-
-
-
-
 class CategoricalTransformer(BaseEstimator, TransformerMixin):
     """Returns data with categorical features handled following a strategy given as argument.
 
@@ -328,70 +296,92 @@ class FeatureSelectorCorr(BaseEstimator, TransformerMixin):
 
 class CustomImputer(BaseEstimator, TransformerMixin):
     """Imputes missing values using a given strategy
-
     @author : RAJHI Mohamed
     """
-    def __init__(self,strategy="mean",missing_values=np.nan):
-        self.strategy=strategy
-        self.missing_values=missing_values
-    
-    
-    
+
+    def __init__(self, strategy="mean", missing_values=np.nan):
+        self.strategy = strategy
+        self.missing_values = missing_values
+
+    def is_scalar_nan(x):
+        """is_scalar_nan(x) is True does not fail.
+        @author : RAJHI Mohamed
+        """
+        return bool(isinstance(x, numbers.Real) and np.isnan(x))
+
+    def _object_dtype_isnan(X):
+        """@author : RAJHI Mohamed"""
+        return X != X
+
+    def _get_mask(X, value_to_mask):
+        """Compute the boolean mask X == value_to_mask.
+           @author : RAJHI Mohamed
+        """
+        if is_scalar_nan(value_to_mask):
+            if X.dtype.kind == "f":
+                return np.isnan(X)
+            elif X.dtype.kind in ("i", "u"):
+                # can't have NaNs in integer array.
+                return np.zeros(X.shape, dtype=bool)
+            else:
+                # np.isnan does not work on object dtypes.
+                return _object_dtype_isnan(X)
+        else:
+            return X == value_to_mask
+
     def fit(self, X, y=None):
         return self
-    
-   
-    
+
     def transform(self, X):
-               
-                X=np.array(X)
-                mask = _get_mask(X, self.missing_values)
-                masked_X = ma.masked_array(X, mask=mask)
 
-                # Mean
-                if self.strategy == "mean":
-                    mean_masked = np.ma.mean(masked_X, axis=0)
-                    mean = np.ma.getdata(mean_masked)
-                    mean[np.ma.getmask(mean_masked)] = np.nan
-                    statistics =mean
-                # Most frequent
-                elif self.strategy == "most_frequent":
-                    X = X.transpose()
-                    mask = mask.transpose()
+        X = np.array(X)
+        mask = _get_mask(X, self.missing_values)
+        masked_X = ma.masked_array(X, mask=mask)
 
-                    if X.dtype.kind == "O":
-                        most_frequent = np.empty(X.shape[0], dtype=object)
-                    else:
-                        most_frequent = np.empty(X.shape[0])
+        # Mean
+        if self.strategy == "mean":
+            mean_masked = np.ma.mean(masked_X, axis=0)
+            mean = np.ma.getdata(mean_masked)
+            mean[np.ma.getmask(mean_masked)] = np.nan
+            statistics = mean
+        # Most frequent
+        elif self.strategy == "most_frequent":
+            X = X.transpose()
+            mask = mask.transpose()
 
-                    for i, (row, row_mask) in enumerate(zip(X[:], mask[:])):
-                        row_mask = np.logical_not(row_mask).astype(np.bool)
-                        row = row[row_mask]
-                        most_frequent[i] = _most_frequent(row, np.nan, 0)
-                    statistics = most_frequent
-                
-                #handling invalid mask
-                invalid_mask = _get_mask(statistics, np.nan)
-                valid_mask = np.logical_not(invalid_mask)
-                valid_statistics = statistics[valid_mask]
-                valid_statistics_indexes = np.flatnonzero(valid_mask)
+            if X.dtype.kind == "O":
+                most_frequent = np.empty(X.shape[0], dtype=object)
+            else:
+                most_frequent = np.empty(X.shape[0])
 
-                if invalid_mask.any():
-                    missing = np.arange(X.shape[1])[invalid_mask]
-                    if self.verbose:
-                        warnings.warn("Deleting features without "
-                                      "observed values: %s" % missing)
-                    X = X[:, valid_statistics_indexes]
+            for i, (row, row_mask) in enumerate(zip(X[:], mask[:])):
+                row_mask = np.logical_not(row_mask).astype(np.bool)
+                row = row[row_mask]
+                most_frequent[i] = _most_frequent(row, np.nan, 0)
+            statistics = most_frequent
 
-                # Do actual imputation
-          
-                mask = _get_mask(X, self.missing_values)
-                n_missing = np.sum(mask, axis=0)
-                values = np.repeat(valid_statistics, n_missing)
-                coordinates = np.where(mask.transpose())[::-1]
+        # handling invalid mask
+        invalid_mask = _get_mask(statistics, np.nan)
+        valid_mask = np.logical_not(invalid_mask)
+        valid_statistics = statistics[valid_mask]
+        valid_statistics_indexes = np.flatnonzero(valid_mask)
 
-                X[coordinates] = values
-                return X
+        if invalid_mask.any():
+            missing = np.arange(X.shape[1])[invalid_mask]
+            if self.verbose:
+                warnings.warn("Deleting features without "
+                              "observed values: %s" % missing)
+            X = X[:, valid_statistics_indexes]
+
+        # Do actual imputation
+
+        mask = _get_mask(X, self.missing_values)
+        n_missing = np.sum(mask, axis=0)
+        values = np.repeat(valid_statistics, n_missing)
+        coordinates = np.where(mask.transpose())[::-1]
+
+        X[coordinates] = values
+        return X
             
  
 
